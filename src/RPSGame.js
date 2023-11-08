6c1684caab90581e9231b63361332ac8309432f0
jpercoco03 <122894264+jpercoco03@users.noreply.github.com>
2023-11-08 15:39:07 -0500

    Initial commit

---
+++ src/RPSGame.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+
+class RPSGame extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userChoice: null,
+      computerChoice: null,
+      result: ""
+    };
+  }
+
+  choices = ["rock", "paper", "scissors"];
+
+  determineWinner = (userChoice, computerChoice) => {
+    if (
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+      return "You Win";
+    } else if (userChoice === computerChoice) {
+      return "It's a tie";
+    } else {
+      return "You Lose";
+    }
+  };
+
+  handleChoice = (choice) => {
+    const userChoice = choice;
+    const computerChoice = this.choices[Math.floor(Math.random() * 3)];
+    const result = this.determineWinner(userChoice, computerChoice);
+
+    this.setState({ userChoice, computerChoice, result });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Rock Paper Scissors</h1>
+        <p>Click on an image to play</p>
+        <div>
+          <button onClick={() => this.handleChoice("rock")}>
+  <img
+    src="https://image.pngaaa.com/44/901044-middle.png"
+    alt="Rock"
+    style={{ width: "100px" }} // Customize image size
+  />
+</button>
+
+          <button onClick={() => this.handleChoice("paper")}>
+  <img
+    src="http://clipart-library.com/images/5TRKqbRqc.png"
+    alt="Paper"
+    style={{ width: "100px" }} // Customize image size
+  />
+</button>
+          <button onClick={() => this.handleChoice("scissors")}>
+  <img
+    src="http://www.clipartbest.com/cliparts/aiq/LBB/aiqLBBLAT.png"
+    alt="Scissors"
+    style={{ width: "100px" }} // Customize image size
+  />
+</button>
+
+        </div>
+        <div>
+          {this.state.userChoice && <p>You chose: {this.state.userChoice}</p>}
+          {this.state.computerChoice && (
+            <p>Your opponent chose: {this.state.computerChoice}</p>
+          )}
+          {this.state.result && <p>Result: {this.state.result}</p>}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RPSGame;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import RPSGame from "./RPSGame";
+import "./styles.css";
+ReactDOM.render(
+  <React.StrictMode>
+    <RPSGame />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
